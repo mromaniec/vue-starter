4055c8ac4dbec498d2ae999a1d512b879106c1ad
romaniec <mnogaj92@gmail.com>
2023-04-16 14:03:35 +0200

    Dodano wpisywanie maila, długości i wyświetlanie w alercie.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <template>
   <div class="my-app">
-    <h1>Hello MWO!</h1>
+    <h1>Twój e-mail to {{ email }}</h1>
+    <input type="email" v-model="email">
+    <div v-if="!email.includes('@')"> To nie jest email!</div>
+    <div v-else-if="email.length < 10"> Adres jest za krótki, bo ma {{email.length}} znaki.</div>
+    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+    <div v-else>Twój adres e-mail jest stanowczo za długi, bo ma {{email.length}} znaków.</div>
+    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      email: 'maromaniec@student.agh.edu.pl',
+      password: ''
+    };
+  },
+  methods: {
+    alertMyEmail() {
+      alert(this.email);
+    }
+  }
 }
 </script>
+
+
 
 <style>
 .my-app {
@@ -17,3 +35,5 @@ export default {
   color: #2c3e50;
 }
 </style>
+
+
