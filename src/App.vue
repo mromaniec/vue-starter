b4b6a85c172503350866d960bdf0bd74ddceb024
romaniec <mnogaj92@gmail.com>
2023-04-16 14:55:02 +0200

    Zmieniono na aplikację do logowania, dodano milligram.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,48 @@
 <template>
   <div class="my-app">
-    <h1>Twój e-mail to {{ email }}</h1>
-    <input type="email" v-model="email">
-    <div v-if="!email.includes('@')"> To nie jest email!</div>
-    <div v-else-if="email.length < 10"> Adres jest za krótki, bo ma {{email.length}} znaki.</div>
-    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
-    <div v-else>Twój adres e-mail jest stanowczo za długi, bo ma {{email.length}} znaków.</div>
-    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
+    <h1>Witaj w systemie do zapisów na zajęcia</h1>
+
+    <div v-if = "loggedIn">
+      Zalogowany na {{email}}
+      <a @click="logMeOut()">Wyloguj</a>
+    </div>
+    <div v-else-if>
+      <br>
+      <label> Zaloguj się emailem: </label>
+      <input type="email" v-model="email">
+      <button @click="logMeIn()">Zaloguj się</button>
+      <div v-if="!email.includes('@')"> To nie jest email!</div>
+      <div v-else-if="email.length < 10"> Adres jest za krótki, bo ma {{email.length}} znaki.</div>
+      <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+      <div v-else>Twój adres e-mail jest stanowczo za długi, bo ma {{email.length}} znaków.</div>
+    </div>
   </div>
 </template>
 
 <script>
+import "milligram";
+
 export default {
   data() {
     return {
-      email: 'maromaniec@student.agh.edu.pl',
+      email: '',
+      loggedIn: false,
       password: ''
     };
   },
   methods: {
     alertMyEmail() {
       alert(this.email);
+    },
+    logMeIn(){
+      this.loggedIn = true;
+    },
+    logMeOut(){
+      this.loggedIn = false;
     }
   }
 }
 </script>
-
-
 
 <style>
 .my-app {
